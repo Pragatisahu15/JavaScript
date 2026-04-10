d09cdf416895709c850a01692ce4448fe63b0e10
Pragati sahu <103757785+Pragatisahu15@users.noreply.github.com>
2026-04-10 23:29:00 +0000

    dataypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict" //treat all Js code as newer version of js 
+
+// alert(3+3) 
+//we'll reference Error: alert's not defined b'cz we r using node.js, not browser
+
+let name="Pragati" //String datatype
+let age=18 //number
+let isLoggedIn = false //boolean datatype
+
+//DataType in Python 
+//Premitive Datatype: number, string, bigint, boolean, null(standalone value(let temp=null)), undefined(variable declared but not assigned), symbol(unique)
+
+//object
+console.log(typeof "Pragati") //string
+console.log(typeof age) //number
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
