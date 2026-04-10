90432d9bb9dcab971175e3f072236fb3f1c965a5
Pragati sahu <103757785+Pragatisahu15@users.noreply.github.com>
2026-04-10 23:28:42 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "pragati@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+// console.log(accountId)
+
+accountEmail = "sahu@gmail.com"
+accountPassword = "212121"
+accountCity = "Pune"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
