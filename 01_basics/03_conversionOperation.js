1612482c7b0fe5f63fcf45555322b792b75490f9
Pragati sahu <103757785+Pragatisahu15@users.noreply.github.com>
2026-04-11 01:18:40 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score = 33
+// let score = "33"
+// let score = "33abc"
+// let score = "sahu"
+// let score = null
+// let score = undefined
+// let score = true
+// let score = false
+
+// console.log(typeof score);
+
+//trying to convert to number
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//trying to convert to boolean
+// let isLoggedIn=1
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+//trying to convert to string
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
